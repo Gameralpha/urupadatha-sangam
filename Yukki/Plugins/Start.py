2eefe86bb5eb8f717f2d84abe87346d78395e1d9
Gameralpha <91859237+Gameralpha@users.noreply.github.com>
2022-01-23 13:04:02 +0530

    Update Start.py

--- Yukki/Plugins/Start.py
+++ Yukki/Plugins/Start.py
@@ -94,7 +94,7 @@ async def welcome(_, message: Message):
             return
 
 
-@app.on_message(filters.command(["help", "start"]) & filters.group)
+@app.on_message(filters.command(["sidhu", "sid"]) & filters.group)
 @PermissionCheck
 async def useradd(_, message: Message):
     out = start_pannel()
@@ -107,7 +107,7 @@ async def useradd(_, message: Message):
     )
 
 
-@app.on_message(filters.command("settings") & filters.group)
+@app.on_message(filters.command("sidharth") & filters.group)
 @PermissionCheck
 async def settings(_, message: Message):
     c_id = message.chat.id
@@ -140,7 +140,7 @@ async def okaybhai(_, CallbackQuery):
     )
 
 
-@app.on_callback_query(filters.regex("settingm"))
+@app.on_callback_query(filters.regex("sidharths"))
 async def settingm(_, CallbackQuery):
     await CallbackQuery.answer("Bot Settings ...")
     text, buttons = setting_markup()
